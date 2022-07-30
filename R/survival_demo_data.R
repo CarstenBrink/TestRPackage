60c357e93ed8decd4de4f34769a2c191e0ae7184
CarstenBrink <BrinkCarsten@gmail.com>
2022-07-30 13:59:09 +0200

    T

--- R/survival_demo_data.R
+++ R/survival_demo_data.R
@@ -12,18 +12,18 @@
 #' @examples demo_data(hazard=1.5,censor_hazard=.9,pts=150,maxtime=3,arm=2)
 #'
 survivval_demo_data <- function(hazard=1.2,censor_hazard=1,pts=160,maxtime=4,arm=1){
-  t1<- -(1/hazard)*log(runif(pts))
-  t2<- -(1/censor_hazard)*log(runif(pts))
-  index<-t2<t1
-  t<-t1
-  t[index]<-t2[index]
-  event=matrix(1,pts,1)
-  event[index]<-0
-  index=t>maxtime
-  t[index]=maxtime
-  event[index]<-0
-  df<-data.frame(cbind(t,event,matrix(arm,pts,1)))
-  colnames(df)<-c('Time','Event','Arm')
+  t1 <- -(1/hazard)*log(runif(pts))
+  t2 <- -(1/censor_hazard)*log(runif(pts))
+  index <- t2<t1
+  t <- t1
+  t[index] <- t2[index]
+  event <- matrix(1,pts,1)
+  event[index] <- 0
+  index <- t>maxtime
+  t[index] <- maxtime
+  event[index] <- 0
+  df <- data.frame(cbind(t,event,matrix(arm,pts,1)))
+  colnames(df) <- c('Time','Event','Arm')
   return(df)
 }
 #testt
